93785c777915e90267014cfc3e303feaf43af3aa
baimu <834300170@qq.com>
2026-04-15 09:17:18 +0800

    fix: 修复左侧列表点击密码文件无法全局解锁的问题

--- src/components/FilePanel.vue
+++ src/components/FilePanel.vue
@@ -76,10 +76,10 @@
       >
         <div class="file-item-top">
           <div class="file-name">
-            <span v-if="store.currentCat === 'c2' && !isGlobalUnlocked()" class="file-star" style="margin-right: 2px;">
+            <span v-if="isPasswordFile(file.id) && !isGlobalUnlocked()" class="file-star" style="margin-right: 2px;">
               <svg width="10" height="10" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/></svg>
             </span>
-            <span v-else-if="store.currentCat === 'c2' && isGlobalUnlocked()" class="file-star" style="margin-right: 2px;">
+            <span v-else-if="isPasswordFile(file.id) && isGlobalUnlocked()" class="file-star" style="margin-right: 2px;">
               <svg width="10" height="10" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"/></svg>
             </span>
             <span class="file-star" v-if="file.starred">★</span>
@@ -250,11 +250,12 @@ function handleAddFile() {
 }
 
 function handleFileClick(file) {
-  if (store.currentCat === 'c2' && isPasswordFile(file.id)) {
+  if (isPasswordFile(file.id)) {
     if (!isGlobalUnlocked()) {
       store.lockFileId = '__global__'
       store.lockMode = 'unlock'
       store.lockCallback = () => {
+        unlockGlobal() // <--- Fix: Mark as unlocked globally
         selectFile(file.id)
       }
       store.lockVisible = true
